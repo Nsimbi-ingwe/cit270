8a33bcf8dcda254dc861834d3be84e61c41d7b95
Nsimbi-ingwe <parkerschmitz@gmail.com>
2022-04-29 13:48:43 -0600

    Added new files

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express'); // imported the library
+
+const app = express(); // used the library
+
+app.listen(3000, ()=>{console.log("listening...")}); // listen
+
+app.get('/', (req,res)=>{res.send("Hello")}); // respond
