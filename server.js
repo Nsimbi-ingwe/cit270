62b1b6caced5067e651011d3ea6fc7fe2108c3b0
Nsimbi-ingwe <parkerschmitz@gmail.com>
2022-05-02 13:40:52 -0600

    Added the port const and put that in the listening print

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express'); // imported the library
 
 const app = express(); // used the library
 
-app.listen(3000, ()=>{console.log("listening...")}); // listen
+const port = 3000;
+
+app.listen(port, ()=>{console.log("listening on Port: " +port)}); // listen
 
 app.get('/', (req,res)=>{res.send("Hello")}); // response
