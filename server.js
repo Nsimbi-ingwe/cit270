f22e02a7cdd2db37f265ecf9c15c3315341a9b53
Nsimbi-ingwe <parkerschmitz@gmail.com>
2022-05-02 13:38:42 -0600

    Added the node server.js start command

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ const app = express(); // used the library
 
 app.listen(3000, ()=>{console.log("listening...")}); // listen
 
-app.get('/', (req,res)=>{res.send("Hello")}); // respond
+app.get('/', (req,res)=>{res.send("Hello")}); // response
