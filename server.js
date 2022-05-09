e20e5af00f253fa0b8998c41c2fb27868136d176
Nsimbi-ingwe <parkerschmitz@gmail.com>
2022-05-09 13:28:40 -0600

    Added code from Bro Murdock

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require('express'); // imported the library
-
 const app = express(); // used the library
-
+const bodyParser = require('body-parser');
 const port = 3000;
 
-app.listen(port, ()=>{console.log("listening on Port: " +port)}); // listen
+app.use(bodyParser.json()); // Use the middleware
 
-app.get('/', (req,res)=>{res.send("Hello")}); // response
+app.listen(port, ()=>{
+    console.log("listening on Port: " +port)}); // listen
+
+app.post('/login', (request, response)=> {
+    const loginRequest = request.body;
+    console.log("request Body", JSON.stringify(request.body));
+    if (loginRequest.userName=="fake@fake.com" && loginRequest.password=="FakePassword1!"){
+        response.status(200); // 200 means ok
+        response.send("Welcome");
+    } else {
+        response.status(401); // 401 means unauthorized
+        response.send("Unauthorized");
+    }
+});
+app.get('/', (request,response)=>{
+    response.send("Hello")}); // response
